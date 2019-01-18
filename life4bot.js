929ca278ba480c0393a8971db4b2ff82fc985996
Steve Sefchick <stevesefchick@gmail.com>
2019-01-17 20:56:57 -0500

    cleanup

--- life4bot.js
+++ life4bot.js
@@ -1,13 +1,9 @@
 //Life4DDRBot
 //Created by Steve Sefchick - 2019
-//Tweets to [tbd]
+//Tweets to @Life4DDRBot
 //build using NodeJS
 
-//Created to tweet player progression and new players joining the @Life4DDR ranking
-
 //update readme
-
-//add express calls
 
 const fs = require('fs');
 var twit = require('twit');
@@ -28,47 +24,13 @@ var getSpreadsheet = function()
     var spreadsheet = require('./spreadsheet.js');
 }
 
-//check file to spreadsheet
-var compareLists = function()
-{
-    var stored_list = fs.readFileSync('stored_list.txt').toString().split("\n");
-    for (i in stored_list){
-        console.log(stored_list[i]);
-    }
-}
-
-
 //check for needed activity
 var life4actionTime = function()
 {
 
     console.log('App is running!!!');
 
-    //compareLists();
-
     getSpreadsheet();
-
-    //tweetNewPlayer();
-
-    //only enable during debug
-    //initPlayerList();
 }
-
-//tweet new
-var tweetNewPlayer = function()
-{
-    //var post = "test tweet!";
-    //Twitter.post('statuses/update', {status: post}, function(err, data, response) {
-    //    console.log(data)
-    //})
-}
-
-//tweet rank up
-var tweetPlayerRankUp = function()
-{
-
-}
-
-
 
 life4actionTime();
